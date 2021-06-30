3bb2ff224728fbc3cdf1235c83f7547af565f669
yichuankaorou <1103287597@qq.com>
2021-06-30 14:06:46 -0700

    code ch4/append

---
+++ ch4/append/practice.go
@@ -0,0 +1,72 @@
+package main
+
+import "fmt"
+
+// Practice, append single param to the end of a slice
+func appendIntPractice(x []int, y int) []int {
+	var z []int
+	// 因为是要追加一个元素到切片中，所以需要 + 1
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
+		// 此时，x 切片仍旧有空间，直接追加即可
+		// z 此时比 x 多了一个元素，此元素为默认值 0，起到占位置作用
+		z = x[:zlen]
+	} else {
+		// 此时，slice 已经没有空间了，需要重新分配一个底层数组
+		// 容量扩展 * 2
+		zcap := zlen
+		if zcap < 2 * len(x) {
+			zcap = 2 * len(x)
+		}
+		// 按照新的 zcap 和 zlen 重新创建 z 切片
+		z = make([]int, zlen, zcap)
+		// 将 x 切片中的元素复制到新 z 中
+		copy(z, x)
+	}
+	// 将 y 复制到上述占位置的元素上，len(x)-1 才是未扩展切片的最后一个元素
+	z[len(x)] = y
+	return z
+}
+
+// Practice, append slice to the end of another slice
+func appendSlicePractice(x []int, y ...int) []int {
+	var z []int
+	zlen := len(x) + len(y)
+	if zlen < cap(x) {
+		// x 中还有足够空间容纳 y 中元素
+		// 占位元素，默认值为 0
+		z = x[:zlen]
+	} else {
+		// x 中没有足够空间容纳 y 中元素
+		// 扩展容量 * 2 倍
+		zcap := zlen
+		if zcap < 2 * len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	// 将 y 复制到上述占位置的元素上，len(x)-1 才是未扩展切片的最后一个元素
+	copy(z[len(x):], y)
+	return z
+}
+
+
+func main() {
+	var x, y []int
+
+	// 追加单个元素到 slice 末尾
+	for i := 0; i < 10; i++ {
+		y = appendIntPractice(x, i)
+		fmt.Printf("%d  cap=%d\t%v\n", i, cap(y), y)
+		x = y
+	}
+
+	// 追加多个元素到 slice 末尾
+	 m := []int{1, 2, 3}
+	 n := []int{4, 5, 6}
+	 var h []int
+
+	 h = appendSlicePractice(m, n...)
+	 fmt.Printf("h is: %v \n", h)
+}
